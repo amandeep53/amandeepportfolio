2f2b63a7be022804478e41fb175c3fa8018a530f
amandeep53 <60456834+amandeep53@users.noreply.github.com>
2020-04-23 01:31:54 -0400

    Update Contact-me.php

--- Contact-me.php
+++ Contact-me.php
@@ -20,22 +20,22 @@
           <nav>
             <ul>
               <li>
-                <a href="index.html">
+                <a href="index.php">
                   <b>HOME</b>
                 </a>
               </li>
               <li>
-                <a href="About-me.html">
+                <a href="About-me.php">
                   <b>ABOUT ME</b>
                 </a>
               </li>
               <li>
-                <a href="My%20Work.html">
+                <a href="My%20Work.php">
                   <b>MY WORK</b>
                 </a>
               </li>
               <li>
-                <a class="active" href="Contact-me.html">
+                <a class="active" href="Contact-me.php">
                   <b>CONTACT ME</b>
                 </a>
               </li>
